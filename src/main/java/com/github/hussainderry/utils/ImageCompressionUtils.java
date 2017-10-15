aeaff7c38f306462e3afa983c4c1c659a2695599
Hussain Al-Derry <hussain.derry@gmail.com>
2017-10-15 11:31:51 +0300

    modified: moved Base64 functionality to ImageCompressor

--- src/main/java/com/github/hussainderry/utils/ImageCompressionUtils.java
+++ src/main/java/com/github/hussainderry/utils/ImageCompressionUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.hussainderry;
+package com.github.hussainderry.utils;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
@@ -37,11 +37,9 @@ public class ImageCompressionUtils {
     private static final String ILLEGAL_COMPRESSION_RATIO = "Illegal compressed image quality (quality must be >= 0 and <= 1)";
     private static final String NULL_BYTES = "imageBytes cannot be null";
     private static final String IMAGE_FORMAT = "jpg";
-    private static Base64.Encoder mEncoder = null;
-    private static Base64.Decoder mDecoder = null;
 
     /**
-     * Compresses an Image using the specified compression ratio
+     * Compresses an Image using the specified compressed image quality
      * @param imageBytes The image bytes to compress
      * @param compressedImageQuality The compressed image quality, must be between 0 and 1 (inclusive)
      * @return byte array containing the compressed image bytes
@@ -79,51 +77,6 @@ public class ImageCompressionUtils {
         }catch(IOException e){
             throw new IllegalStateException(e);
         }
-    }
-
-    /**
-     * Compresses an Image to JPG using the specified compression ratio
-     * @param imageBase64 The image bytes encoded as Base64
-     * @param compressedImageQuality The compressed image quality, The compressed image quality, must be between 0 and 1 (inclusive)
-     * @return byte array containing the compressed image bytes
-     * @throws IllegalStateException if an error occurs
-     * @throws IllegalArgumentException if there is an illegal argument
-     */
-    public static byte[] compressImage(String imageBase64, float compressedImageQuality){
-        if(mDecoder == null){
-            mDecoder = Base64.getDecoder();
-        }
-        return compressImage(mDecoder.decode(imageBase64), compressedImageQuality);
-    }
-
-    /**
-     * Compresses an Image to JPG using the specified compression ratio
-     * @param imageBytes The image bytes to compress
-     * @param compressedImageQuality The compressed image quality, The compressed image quality, must be between 0 and 1 (inclusive)
-     * @return Base64 representation of the compressed image
-     * @throws IllegalStateException if an error occurs
-     * @throws IllegalArgumentException if there is an illegal argument
-     */
-    public static String compressImageBase64(byte[] imageBytes, float compressedImageQuality){
-        if(mEncoder == null){
-            mEncoder = Base64.getEncoder();
-        }
-        return mEncoder.encodeToString(compressImage(imageBytes, compressedImageQuality));
-    }
-
-    /**
-     * Compresses an Image to JPG using the specified compression ratio
-     * @param imageBase64 The image bytes encoded as Base64
-     * @param compressedImageQuality The compressed image quality, The compressed image quality, must be between 0 and 1 (inclusive)
-     * @return Base64 representation of the compressed image
-     * @throws IllegalStateException if an error occurs
-     * @throws IllegalArgumentException if there is an illegal argument
-     */
-    public static String compressImageBase64(String imageBase64, float compressedImageQuality){
-        if(mDecoder == null){
-            mDecoder = Base64.getDecoder();
-        }
-        return compressImageBase64(mDecoder.decode(imageBase64), compressedImageQuality);
     }
 
     private static boolean isValidImageQuality(float ratio){
